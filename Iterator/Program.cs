44f752f3cede491682842e32ecd95c11323e6752
karolaaaxd <karolciaxd2011@onet.pl>
2021-04-10 16:27:23 +0200

    Iterator

---
+++ Iterator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Iterator
+{
+    class Program
+
+    {
+        class Iterator
+        {
+            public static IEnumerable<string> GetArray()
+            {
+                int[] tablica = new int[] { 5, 7, 6, 9, 1 };
+                
+                foreach (var element in tablica)
+                {
+                    yield return element.ToString(); 
+                }
+            }
+            public static void Main(string[] args)
+            {
+                IEnumerable<string> elements = GetArray();
+                foreach (var element in elements) 
+                {
+                    Console.WriteLine(element);
+                }
+            }
+        }
+    }
+}
